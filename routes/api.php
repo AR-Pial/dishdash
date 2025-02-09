46d8e0e967bd459664d305b97b232c9936880178
AR-Pial <arpialcodes@gmail.com>
2025-02-09 15:52:27 +0600

    tables table needed

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,9 @@
 <?php
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\TableController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\SubCategoryController;
 
-Route::apiResource('table', TableController::class);
 Route::apiResource('category', CategoryController::class);
 Route::apiResource('sub-category', SubCategoryController::class);
 
